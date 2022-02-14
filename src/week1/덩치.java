373e547f01eefe7e901215cf6bbbe89c231a4240
bang jihoon <jihoon289@naver.com>
2022-02-14 15:08:25 +0900

    ex2

--- src/week1/덩치.java
+++ src/week1/덩치.java
@@ -1,7 +1,63 @@
 package week1;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
 public class 덩치 {
-    public static void main(String[] args) {
-        System.out.println("test123");
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st;
+        // 정보 초기화
+        Person[] persons = new Person[N];
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine(), " ");
+            int weigth = Integer.parseInt(st.nextToken());
+            int height = Integer.parseInt(st.nextToken());
+            persons[i] = new Person(i,weigth,height);
+        }
+        // Grade 매기기
+        Arrays.sort(persons, (o1, o2) -> o2.weigth - o1.weigth);// 몸무게 높은순
+        persons[0].grade += 1;
+        for(int i=0;i<N-1;i++) {
+            if (persons[i].weigth == persons[i + 1].weigth)
+                persons[i + 1].grade += persons[i].grade;
+            else
+                persons[i + 1].grade += i+2;
+        }
+        Arrays.sort(persons, (o1, o2) -> o2.height - o1.height);//키 높은순
+        persons[0].grade += 1;
+        for(int i=0;i<N-1;i++) {
+            if (persons[i].height == persons[i + 1].height)
+                persons[i + 1].grade += persons[i].grade;
+            else
+                persons[i + 1].grade += i+2;
+        }
+        Arrays.sort(persons, (o1, o2) -> o1.rank - o2.rank); //랭크값 낮은순
+        persons[0].rank = 1;
+        for(int i=1;i<N;i++) {
+            if (persons[i-1].grade == persons[i].grade)
+                persons[i].rank = persons[i-1].rank;
+            else
+               persons[i].rank = i+1;
+        }
+        // 결과 출력
+        Arrays.sort(persons, (o1, o2) -> o1.i - o2.i); //낮은순
+        for (Person person : persons) System.out.print(person.rank + " ");
     }
 }
+class Person{
+    int i, weigth, height, grade=0, rank;
+    public Person(int i, int weigth, int height) {
+        this.i = i;
+        this.weigth = weigth;
+        this.height = height;
+    }
+}
+/*
+* 잘못된 접근을 했다. 키가 2등이건 5등이건, 그 사람이 등치 rank랑은 상관이 없었다;;
+* 단순히 키랑 무게가 둘다 큰 사람은 큰걸로 간주하고, 비교가 애매하면 동 랭크였는데..
+* */
---
+++ src/week1/덩치2.java
@@ -0,0 +1,37 @@
+package week1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class 덩치2 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[][] arr = new int[N][2];
+        String[] str;
+        for(int i = 0; i < N; i++) {
+            str = br.readLine().split(" ");
+            arr[i][0] = Integer.parseInt(str[0]);	// [i][0] : 몸무게
+            arr[i][1] = Integer.parseInt(str[1]);	// [i][1] : 키
+        }
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < N; i++) {
+            int rank = 1;
+            for(int j = 0; j < N; j++) {
+                if (i == j) continue;
+                if (arr[i][0] < arr[j][0] && arr[i][1] < arr[j][1]) {
+                    rank++;
+                }
+            }
+            sb.append(rank).append(' ');
+        }
+		System.out.println(sb);
+    }
+}
+/*
+* 쉬운걸 왜 어렵게 접근했을까...
+* 정말 단순히 input이 50개 밖에 안되니깐 배열에 넣고,
+* 자기보다 큰게 있을때마다 ++로 랭크를 하나씩 낮춰주면서 붙여주면되는데..
+* 왜 Class를 만들고, Sorting을 해주면서 계산했는가..;;
+* */
