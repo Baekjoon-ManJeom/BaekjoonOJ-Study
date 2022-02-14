7f552b72b4b56c30979cf95fdcae1bb67f63a0e4
TaeJeong <xowjd1013@naver.com>
2022-02-14 23:00:13 +0900

    백준 2798

--- src/backjoon2798.java
+++ src/backjoon2798.java
@@ -27,13 +27,15 @@ public class backjoon2798 {
 	static int result(int[] t, int n, int m) {
 		int answer = 0;
 		
-		
+		// t[i],t[j],t[k]가 중복된 인덱스를 가리키지 않도록 for문을 구성했다.
 		for(int i = 0; i<t.length-2;i++) {
-			for(int j = 1+i; j<t.length-1;j++) {
+			for(int j = 1+i; j<t.length-1;j++) {   
 				for(int k = 1+j; k<t.length;k++) {
-
+					if(t[i]+t[j]+t[k] == m) {
+						return t[i]+t[j]+t[k];  // 3수의 합이 m과 같다면 더 이상 탐색할 필요가 없다.
+					}
 					
-					if(answer<t[i]+t[j]+t[k] && t[i]+t[j]+t[k] <=m) {
+					if(answer<t[i]+t[j]+t[k] && t[i]+t[j]+t[k] <m) {  // 완전 탐색을 통해 더 큰 값이 나오면 answer값을 교체한다
 						answer = t[i]+t[j]+t[k];
 					}
 					
