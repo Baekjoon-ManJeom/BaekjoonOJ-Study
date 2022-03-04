3cda57510b3786b985d1613b16d469dcb50a76d5
Bang(home) <jihoon289@naver.com>
2022-03-04 21:50:06 +0900

    week3 - 백트래킹

---
+++ src/week3/N과M1.java
@@ -0,0 +1,39 @@
+package week3;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class N과M1 {
+    // 중복 가능한 조합을 만드는 문제
+    static int n,m;
+    public static void main(String[] args) {
+        // 입력
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        m = sc.nextInt();
+        sc.close();
+        // 조합 출력
+        Boolean chosen[] = new Boolean[n+1];
+        Arrays.fill(chosen,false);
+        for (int i = 1; i <= n; i++) {
+            chosen[i] = true;
+            combi(chosen,1,i+"");
+            chosen[i] = false;
+        }
+    }
+    // 조합 출력  (선택값체크배열,
+    public static void combi(Boolean[] chosen, int depth, String str){
+        if(m == depth){
+            System.out.println(str);
+        }
+        else{
+            for (int i = 1; i <=n ; i++) {
+                if(!chosen[i]){
+                    chosen[i] = true;
+                    combi(chosen,depth+1,str+" "+i);
+                    chosen[i] = false;
+                }
+            }
+        }
+    }
+}
