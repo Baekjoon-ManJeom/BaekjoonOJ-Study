4667b4287bb8189e6310ddb10653627e77b2061c
Bang(home) <jihoon289@naver.com>
2022-03-05 00:06:04 +0900

    solve 2 Ex

---
+++ src/week3/스타드와링크.java
@@ -0,0 +1,7 @@
+package week3;
+
+public class 스타드와링크 {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/week3/연산자끼워넣기.java
@@ -0,0 +1,7 @@
+package week3;
+
+public class 연산자끼워넣기 {
+    public static void main(String[] args) {
+
+    }
+}
