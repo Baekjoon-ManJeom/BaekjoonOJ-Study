e4dc3fc7c609581f2a5cd4b019006f89c36c8902
Bangjihoon <jihoon289@naver.com>
2022-03-29 15:32:08 +0900

    week7 - Solve 큐2 : 데크를 이용한 풀이

--- src/week7/큐2.java
+++ src/week7/큐2.java
@@ -4,12 +4,14 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayDeque;
-
+// 빡세다 기준이... 반드시 BuffeeredReader와 StringBuilder 로 in/put하자
+// 393,348 KB , 1520 ms
 public class 큐2 {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException  {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
         ArrayDeque<String> dq = new ArrayDeque<>();
+        StringBuilder sb = new StringBuilder();
         for (int i = 0; i < n; i++) {
             String cmd = br.readLine();
             switch (cmd.split(" ")[0]){
@@ -18,32 +20,55 @@ public class 큐2 {
                     break;
                 case "pop":
                     if(dq.isEmpty()){
-                        System.out.println("-1");
+                        sb.append("-1").append("\n");
                     }else
-                        System.out.println(dq.pollLast());
+                        sb.append(dq.pollLast()).append("\n");
                     break;
                 case "empty":
                     if(dq.isEmpty())
-                        System.out.println(1);
+                        sb.append("1").append("\n");
                     else
-                        System.out.println(0);
+                        sb.append("0").append("\n");
                     break;
                 case "size":
-                    System.out.println(dq.size());
+                    sb.append(dq.size()).append("\n");
                     break;
                 case "front" :
                     if(dq.isEmpty())
-                        System.out.println("-1");
+                        sb.append("-1").append("\n");
                     else
-                        System.out.println(dq.pollLast());
+                        sb.append(dq.peekLast()).append("\n");
                     break;
                 case "back":
                     if(dq.isEmpty())
-                        System.out.println("-1");
+                        sb.append("-1").append("\n");
                     else
-                        System.out.println(dq.pollFirst());
+                        sb.append(dq.peekFirst()).append("\n");
                     break;
             }
         }
+        System.out.println(sb);
+        br.close();
     }
 }
+
+/*
+
+15
+push 1
+push 2
+front
+back
+size
+empty
+pop
+pop
+pop
+size
+empty
+pop
+push 3
+empty
+front
+
+ */
