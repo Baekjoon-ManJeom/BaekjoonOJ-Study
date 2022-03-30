7ddeaac5bc44fa15b5f5f95439ae546d0cf2a57e
Bangjihoon <jihoon289@naver.com>
2022-03-30 10:29:34 +0900

    week7 - Solve 카드 : AC 시간초과 오답풀이2

--- src/week7/AC.java
+++ src/week7/AC.java
@@ -10,51 +10,64 @@ public class AC {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int T = Integer.parseInt(br.readLine());
         StringBuilder sb = new StringBuilder();
-        Queue<Integer> q = new LinkedList<>();
+        Queue<String> q = new LinkedList<>();
         for (int i = 0; i < T; i++) {
             // init
             boolean err = false; // 에러여부 체크
             boolean order = true; // 방향 초기화
-            q.clear(); // 큐  비우기
+            q.clear();
             // input
             String p = br.readLine(); // p 문자열
-            int n = Integer.parseInt(br.readLine()); // 배열 크기 (사용안함)
-            String [] tmp = br.readLine().replaceAll("[^0-9,]", "").split(",");
-            int [] arr = new int[n];
-            if(n!=0)
-                arr = Arrays.stream(tmp).mapToInt(Integer::parseInt).toArray();
-            for (int s : arr) q.add(s);
-
+            String n = br.readLine(); // 배열 크기 (사용안함)
+            StringTokenizer st = new StringTokenizer(br.readLine(), "[],");
+            // 준비
+            while(st.hasMoreTokens())
+                q.add(st.nextToken());
             // 연산
-            L1:
             for (char c : p.toCharArray()) {
-                switch(c){
-                    case 'R':
-                        order = !order;
+                if (c == 'R') {
+                    order = !order;
+                    continue;
+                }
+                if (c == 'D') {
+                    if (q.isEmpty()) {
+                        err = true;
                         break;
-                    case 'D':
-                        if(q.isEmpty()){
-                            err = true;
-                            sb.append("error\n");
-                            break L1;
-                        }
-                        if(order) q.poll();
-                        else q.remove(q.size());
-                        break;
+                    }
+                    if(order)
+                        q.poll();
+                    else q.remove(Integer.toString(q.size()));
                 }
             }
             // 결과저장
             if(!err) {
-                List<Integer> collect = new ArrayList<>(q);
                 if (order)
                     sb.append(q).append("\n");
                 else{
+                    // 여기가 시간을 쓰는듯?
+                    List<String> collect = new ArrayList<>(q);
                     Collections.reverse(collect);
                     sb.append(collect).append("\n");
                 }
+            }else{
+                sb.append("error\n");
             }
         }
         System.out.println(sb);
         br.close();
     }
 }
+/*
+4
+RDD
+4
+[1,2,3,4]
+DD
+1
+[42]
+RRD
+6
+[1,1,2,3,5,8]
+D
+0
+[]*/
