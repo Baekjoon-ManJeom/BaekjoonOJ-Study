53e07a4fa07d705e2f767ca5cafceba5874d47df
BangJihoon <jihoon289@naver.com>
2022-04-14 17:57:53 +0900

    week8 - init 종이의개수

--- src/week8/종이의개수.java
+++ src/week8/종이의개수.java
@@ -1,4 +1,23 @@
 package week8;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class 종이의개수 {
+    static int[][] arr;
+    static int flag;
+    static StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        arr = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            String tmp = br.readLine();
+            for (int j = 0; j < n; j++) {
+                arr[i][j] = Character.getNumericValue(tmp.charAt(j));
+            }
+        }
+        br.close();
+    }
 }
