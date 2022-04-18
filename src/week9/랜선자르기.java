656b23371a605f4955392fef6ccbe68d250457d5
BangJihoon <jihoon289@naver.com>
2022-04-18 18:00:27 +0900

    week9 - Solved 랜선자르기

--- src/week9/랜선자르기.java
+++ src/week9/랜선자르기.java
@@ -1,4 +1,43 @@
 package week9;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
 public class 랜선자르기 {
+    static int[] arr;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+        st = new StringTokenizer(br.readLine()," ");
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        arr = new int[n];
+        for (int i=0;i<n;i++){
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+        Arrays.sort(arr);
+        System.out.println(upper_bound(m));
+        br.close();
+    }
+    static long upper_bound(int m){
+        long l=0;
+        long r=arr[arr.length-1]+1; // 0으로 나누는 불상사를 막기 위함
+        long sum, mid;
+        while(l<r){
+            sum=0;
+            mid = (l+r)/2;
+            for(int i = 0; i < arr.length; i++) {
+                sum += arr[i]/mid; // 여기때문에 r+1로 탐색 시작
+            }
+            if(sum<m)
+                r = mid;
+            else
+                l = mid +1;
+        }
+        return l-1;
+    }
 }
